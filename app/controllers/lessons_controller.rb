62c3d2c1e5f642cd3ff3476959c13131798f697c
Lesley Park <lesparkley@gmail.com>
2016-11-06 18:51:47 +0000

    Set up lessons permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_to_show_lesson, :only => [:show]
   def show
   end
 
@@ -6,5 +8,11 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_enrolled_to_show_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course) && current_lesson.section.course.user != current_user
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled to view lessons!'
+    end
   end
 end
